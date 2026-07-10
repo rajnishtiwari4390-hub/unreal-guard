a5c00d51d4a7af51ce6ffe77ade35923deebfc46
rajnishtiwari4390-hub <rajnishtiwari4390@gmail.com>
2026-07-10 11:56:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from telegram import (
+    Update,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup
+)
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    CallbackQueryHandler,
+    ContextTypes
+)
+import os
 
+TOKEN = os.getenv("BOT_TOKEN")
+
+OWNER_ID = 8656839796
+ADMINS = [8656839796, 7085991794]
