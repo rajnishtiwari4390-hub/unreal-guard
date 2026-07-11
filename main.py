f019465e1cca1890209cc688db4ffc0863fe238e
rajnishtiwari4390-hub <rajnishtiwari4390@gmail.com>
2026-07-11 09:51:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 from telegram import Update
-from telegram.ext import (
-    Application,
-    CommandHandler,
-    ContextTypes,
-)
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 from config import BOT_TOKEN
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "🔥 UNREAL GUARD BOT ONLINE 🔥\n\n"
-        "Use /help to see commands."
+        "🔥 UNREAL GUARD BOT ONLINE 🔥\n\nUse /help to see commands."
     )
+
 
 async def help_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        """
-Available Commands
+        """Available Commands
 
 /start
 /help
@@ -30,10 +26,16 @@ Available Commands
 """
     )
 
-app = Application.builder().token(BOT_TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("help", help_cmd))
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
 
-print("Bot Started...")
-app.run_polling()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("help", help_cmd))
+
+    print("Bot Started...")
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
