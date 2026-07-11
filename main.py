6d578b76dfd2a6ed2e46e7899629a59562c07010
rajnishtiwari4390-hub <rajnishtiwari4390@gmail.com>
2026-07-11 09:14:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+)
 
+from config import BOT_TOKEN
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🔥 UNREAL GUARD BOT ONLINE 🔥\n\n"
+        "Use /help to see commands."
+    )
+
+async def help_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        """
+Available Commands
+
+/start
+/help
+
+(Admin)
+/ban
+/kick
+/mute
+/warn
+/unmute
+"""
+    )
+
+app = Application.builder().token(BOT_TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("help", help_cmd))
+
+print("Bot Started...")
+app.run_polling()
